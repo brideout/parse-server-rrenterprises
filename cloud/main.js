30f4cfee40efff0dd062faa3957ad77431ee628c
brideout <bret@rapidwarellc.com>
2017-12-05 23:00:51 -0600

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -3,27 +3,6 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
 
-Parse.Cloud.define("testJob", function(request, response) {
-  var GameScore = Parse.Object.extend("GameScore");
-  var gameScore = new GameScore();
-
-  gameScore.set("score", 1337);
-  gameScore.set("playerName", "Sean Plott");
-  gameScore.set("cheatMode", false);
-
-  gameScore.save(null, {
-    success: function(gameScore) {
-      // Execute any logic that should take place after the object is saved.
-      alert('New object created with objectId: ' + gameScore.id);
-    },
-    error: function(gameScore, error) {
-      // Execute any logic that should take place if the save fails.
-      // error is a Parse.Error with an error code and message.
-      alert('Failed to create new object, with error code: ' + error.message);
-    }
-  });
-}
-
 Parse.Cloud.define("updateExpDate", function(request, response){
   var query = new Parse.Query(Parse.User);
   query.equalTo('customer_id', request.params.customerId);
