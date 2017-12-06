bc437b73f0c50b091345da06eb25c26991ee5fbb
brideout <bret@rapidwarellc.com>
2017-12-05 23:19:07 -0600

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -3,6 +3,27 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
 
+Parse.Cloud.job("myJob", function(request, status) {
+  // the params passed through the start request
+  const params = request.params;
+  // Headers from the request that triggered the job
+  const headers = request.headers;
+
+  // get the parse-server logger
+  const log = request.log;
+
+  // Update the Job status message
+  status.message("I just started");
+  doSomethingVeryLong().then(function(result) {
+    // Mark the job as successful
+    // success and error only support string as parameters
+    status.success("I just finished");
+  })
+  .catch(function(error) {
+    // Mark the job as errored
+    status.error("There was an error");
+  });
+});
 
  Parse.Cloud.define("averageStars", function(request, response) {
   var Review = Parse.Object.extend("Skus");
